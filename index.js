41c94793f28739a82d0849b6dc32b55ff7f20d9e
Kylehinks97 <kyle.s.hinks@outlook.com>
2023-08-13 11:40:11 +0100

    add randomised data call

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const pool = require("./db")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
 
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //ROUTES//
 
 // create a todo
-app.get("/verbs", async(req, res) => {
-    try {
-        const allData = await pool.query("SELECT * FROM verbs;")
-        res.json(allData.rows)
-    } catch (err) {
-        console.error(err.message)
-    }
-})
+app.get("/new-lesson", async (req, res) => {
+  try {
+    const randomizedData = await pool.query(`
+            WITH RandomizedRows AS (
+                SELECT
+                    id,
+                    verb_id,
+                    subject_pronoun,
+                    conjugated_form,
+                    kata,
+                    ROW_NUMBER() OVER (PARTITION BY verb_id ORDER BY random()) AS rn
+                FROM conjugations
+            )
+            SELECT
+                id,
+                verb_id,
+                subject_pronoun,
+                conjugated_form,
+                kata
+            FROM RandomizedRows
+            WHERE rn = 1;
+        `);
+    res.json(randomizedData.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 // get all todos
 
 // get a todo
@@ -27,5 +47,5 @@ app.get("/verbs", async(req, res) => {
 // delete a todo
 
 app.listen(5000, () => {
-    console.log("server listening on port 5000 :)");
-})
+  console.log("server listening on port 5000 :)");
+});
