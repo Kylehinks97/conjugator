ae0eef8628abcae3bd2e931c056275327349d63f
Kylehinks97 <kyle.s.hinks@outlook.com>
2023-08-14 20:01:25 +0100

    add changes to insertions and front end

--- frontend/conjugator/src/PresentLesson.tsx
+++ frontend/conjugator/src/PresentLesson.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState, useRef } from "react";
 import "./styles/PresentLesson.css";
 import newLesson from "./fetchers";
 import { Kata } from "./types.ts";
+import { BsCheckCircleFill, BsXCircleFill } from "react-icons/bs";
+import { GB, ES } from "country-flag-icons/react/3x2";
 
 function PresentLesson() {
   const [katas, setKatas] = useState<Kata[]>([]);
@@ -16,6 +18,7 @@ function PresentLesson() {
   const [score, setScore] = useState<number>(0);
   const [inQuestion, setInQuestion] = useState<boolean | null>(null);
   const [isCheckDisabled, setIsCheckDisabled] = useState<boolean>(true);
+  const [isOptionsDisabled, setIsOptionsDisabled] = useState<boolean>(false);
   const [showCorrectMessage, setShowCorrectMessage] = useState<boolean | null>(
     null
   );
@@ -23,11 +26,24 @@ function PresentLesson() {
     boolean | null
   >(null);
   const [correctAnswers, setCorrectAnswers] = useState<string[]>([]);
+  const [correctSentences, setCorrectSentences] = useState<string[]>([]);
   const [isAnswered, setIsAnswered] = useState<boolean>(false);
+  const [selectedChoiceIdx, setSelectedChoiceIdx] = useState<null | number>(
+    null
+  );
+  const [translations, setTranslations] = useState<string[]>([]);
 
-  console.log(inQuestion, "<-- inQuestion");
-  console.log(readyToContinue, "<-- readyToContinue");
-  console.log(selected, "<-- selected");
+  function getSentences() {
+    katas.forEach((kata) => {
+      setCorrectSentences((previousSentences) => [
+        ...previousSentences,
+        kata.kata.replace("___", kata.conjugated_form),
+      ]);
+    });
+  }
+  
+  // console.log(correctSentences, "correctSentences");
+ console.log(translations, "translations");
 
   useEffect(() => {
     if (isInitialRender.current) {
@@ -37,10 +53,14 @@ function PresentLesson() {
     if (newLessonLaunched) {
       newLesson().then((result: any) => {
         const lessonData = result.data.map((kata: any) => {
+          console.log(kata);
           setCorrectAnswers((prevCorrectAnswers) => [
             ...prevCorrectAnswers,
             kata.conjugated_form,
           ]);
+          setTranslations((prevTranslations) => [
+            ...prevTranslations, kata.translation
+          ])
           const choices = [
             ...`${kata.conjugated_form}, ${kata.options}`.split(", "),
           ].sort(() => Math.random() - 0.5);
@@ -51,9 +71,16 @@ function PresentLesson() {
     }
   }, [newLessonLaunched]);
 
-  console.log(correctAnswers);
+  useEffect(() => {
+    if (correctAnswers.length > 0 && katas.length === correctAnswers.length) {
+      const sentences = katas.map((kata, index) =>
+        kata.kata.replace("___", correctAnswers[index])
+      );
+      setCorrectSentences(sentences);
+    }
+  }, [correctAnswers, katas]);
 
-  function handleOptionClick(chosen: any, correct: any) {
+  function handleOptionClick(chosen: any, correct: any, choiceIdx: number) {
     if (chosen === correct && !isAnswered) {
       setIsCorrect(true);
       setIsAnswered(true);
@@ -62,6 +89,7 @@ function PresentLesson() {
     }
     setSelected(chosen);
     setIsCheckDisabled(false);
+    setSelectedChoiceIdx(choiceIdx);
   }
 
   function showCorrect() {
@@ -74,81 +102,138 @@ function PresentLesson() {
     setShowIncorrectMessage(true);
   }
 
+
   return (
-    <div>
-      <h1 className="text-xl font-bold text-blue-500">conjugator</h1>
-      <button
-        onClick={() => {
-          setNewLessonLaunched(true);
-          setInQuestion(true);
-        }}
-      >
-        New Lesson
-      </button>
-      {newLessonLaunched &&
-        katas.length > 0 &&
-        currentKataIndex < katas.length && (
-          <div key={currentKataIndex}>
-            <p>Score {score}</p>
-            <p>
-              {selected === null
-                ? katas[currentKataIndex].kata
-                : katas[currentKataIndex].kata.replace("___", `${selected}`)}
-              <br />
-              <br />
-            </p>
-            {katas[currentKataIndex].choices.map(
-              (choice: string, choiceIdx: number) => (
-                <button
-                  key={choiceIdx}
-                  onClick={() => {
-                    handleOptionClick(
-                      choice,
-                      katas[currentKataIndex].conjugated_form
-                    );
-                  }}
-                  className={`option-button`}
-                >
-                  {choice}
-                </button>
-              )
-            )}
-          </div>
-        )}
-      {newLessonLaunched && !readyToContinue && inQuestion ? (
-        <button
-          disabled={isCheckDisabled}
-          onClick={() => {
-            isCorrect ? showCorrect() : showIncorrect();
-            setInQuestion(false);
-            isCorrect ? setScore(score + 1) : null;
-          }}
-          className={`check-button ${!selected ? "" : "correct"}`}
-        >
-          CHECK
-        </button>
-      ) : null}
-      {readyToContinue && !inQuestion ? (
+    <>
+      <div className="container">
+        <h1 className="text-xl font-bold text-blue-500">conjugator</h1>
         <button
           onClick={() => {
-            setSelected(null);
-            setCurrentKataIndex((prevIndex) => prevIndex + 1);
+            setNewLessonLaunched(true);
             setInQuestion(true);
-            setReadyToContinue(false);
-            setIsCheckDisabled(true);
-            setShowCorrectMessage(false);
-            setShowIncorrectMessage(false);
-            setIsAnswered(false);
+            getSentences();
           }}
         >
-          CONTINUE
+          New Lesson
         </button>
-      ) : null}
-      {showCorrectMessage ? <p>Well done! The answer is {selected}</p> : null}
+        {newLessonLaunched &&
+          katas.length > 0 &&
+          currentKataIndex < katas.length && (
+            <div key={currentKataIndex}>
+              <p>Score {score}</p>
+              <p className="kata-text">
+                {selected === null
+                  ? katas[currentKataIndex].kata
+                  : katas[currentKataIndex].kata.replace("___", `${selected}`)}
+              </p>
+              {katas[currentKataIndex].choices.map(
+                (choice: string, choiceIdx: number) => (
+                  <button
+                    disabled={isOptionsDisabled}
+                    key={choiceIdx}
+                    onClick={() => {
+                      handleOptionClick(
+                        choice,
+                        katas[currentKataIndex].conjugated_form,
+                        choiceIdx
+                      );
+                    }}
+                    className={`option-button ${
+                      selectedChoiceIdx === choiceIdx ? "selected" : ""
+                    }`}
+                  >
+                    {choice}
+                  </button>
+                )
+              )}
+            </div>
+          )}
+        {newLessonLaunched && !readyToContinue && inQuestion ? (
+          <div>
+            <button
+              disabled={isCheckDisabled}
+              onClick={() => {
+                isCorrect ? showCorrect() : showIncorrect();
+                setInQuestion(false);
+                isCorrect ? setScore(score + 1) : null;
+                setIsOptionsDisabled(true);
+              }}
+              className={`progress-button ${
+                !selected ? "" : "correct-background"
+              }`}
+            >
+              CHECK
+            </button>
+          </div>
+        ) : null}
+        {readyToContinue && !inQuestion ? (
+          <button
+            onClick={() => {
+              setIsOptionsDisabled(false);
+              setSelected(null);
+              setCurrentKataIndex((prevIndex) => prevIndex + 1);
+              setInQuestion(true);
+              setReadyToContinue(false);
+              setIsCheckDisabled(true);
+              setShowCorrectMessage(false);
+              setShowIncorrectMessage(false);
+              setIsAnswered(false);
+            }}
+            className={`progress-button ${
+              isCorrect ? "correct-background" : "incorrect-background"
+            }`}
+          >
+            CONTINUE
+          </button>
+        ) : null}
+      </div>
       {showIncorrectMessage ? (
-        <p>Oops! The answer is {correctAnswers[currentKataIndex]}</p>
+        <div className="answered-message">
+          <p className="incorrect" id="incorrect-message">
+            <span className="message-text">
+              <BsXCircleFill className="icon" />
+            </span>
+            Oops! The answer is {correctAnswers[currentKataIndex]}
+          </p>
+          <p className="correct" id="incorrect-message">
+            <p id="incorrect-message">
+              <div className="flag-container">
+                <ES id="flag" />
+              </div>
+            </p>
+            {correctSentences[currentKataIndex]}
+          </p>
+          <p id="incorrect-message">
+            <div className="flag-container">
+              <GB id="flag" />
+            </div>
+          </p>
+        </div>
       ) : null}
-    </div>
+      {showCorrectMessage ? (
+        <div className="answered-message">
+          <p className="correct" id="correct-message">
+            <span className="message-text">
+              <BsCheckCircleFill className="icon" />
+            </span>
+            Well done! The answer is {selected}
+          </p>
+          <p className="correct" id="incorrect-message">
+            <p id="incorrect-message">
+              <div className="flag-container">
+                <ES id="flag" />
+              </div>
+            </p>
+            {correctSentences[currentKataIndex]}
+          </p>
+          <p id="incorrect-message">
+            <div className="flag-container">
+              <GB id="flag" />
+            </div>
+          </p>
+        </div>
+      ) : null}
+    </>
   );
 }
 
